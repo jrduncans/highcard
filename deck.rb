a118b342b953511bfb3396e936a7781a2c104001
stephen.duncan <stephen.duncan@f191c422-131a-0410-8d44-f1ddf0d51770>
2006-08-03 01:59:49 +0000

    Inital commit of highcard game

---
+++ deck.rb
@@ -0,0 +1,45 @@
+# Copyright 2006 Stephen Duncan Jr
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "card"
+
+class Deck
+	attr_reader :cards
+	
+	def initialize
+		@cards = []
+		@drawnCards = []
+		@index = 0
+		Card.ranks.each { |rank| Card.suits.each { |suit| @cards.push(Card.new(rank, suit)) } }
+	end
+	
+	def draw	
+		card = @cards[@index]
+		
+		if @drawnCards.include? card
+			result = draw
+		else
+			@drawnCards.push(card)
+			result = card
+		end
+		
+		@index += 1 unless @index >= (Card.ranks.length - 1)
+		
+		return result
+	end
+	
+	def reset
+		@drawnCards = []
+		@index = 0
+	end
+end
