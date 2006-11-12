af9a91aba06b01383f5dadaffcb7c580e7daa0c1
stephen.duncan <stephen.duncan@f191c422-131a-0410-8d44-f1ddf0d51770>
2006-11-12 22:48:10 +0000

    Added tests and Rakefile for ruby version

---
+++ ruby/test/cards_test.rb
@@ -0,0 +1,45 @@
+#!/bin/env ruby
+
+# Copyright 2006 Stephen Duncan Jr
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'test/unit'
+require 'cards'
+
+class CardsTest < Test::Unit::TestCase
+	def setup
+		@ace_of_clubs = Cards::Card.new('Ace', 'Clubs')
+		@ace_of_spades = Cards::Card.new('Ace', 'Spades')
+		@two_of_clubs = Cards::Card.new(2, 'Clubs')
+		@three_of_diamonds = Cards::Card.new(3, 'Diamonds')
+	end
+
+	def test_equality
+		ace_of_clubs = Cards::Card.new('Ace', 'Clubs')
+
+		assert_equal(@ace_of_clubs, ace_of_clubs)
+		assert_not_equal(@ace_of_clubs, @ace_of_spades)
+		assert_not_equal(@ace_of_clubs, @two_of_clubs)
+	end
+
+	def test_comparison
+		assert(@ace_of_clubs < @two_of_clubs, 'Ace not greater than or equal to two' )
+		assert(@three_of_diamonds > @two_of_clubs, 'Two not less than or equal to three')
+
+		assert_equal(@ace_of_clubs <=> @ace_of_spades, 0, 'Ace of Clubs not greater than or less than Ace of Spades')
+	end
+
+	def test_tostring
+		assert_match(/\bace\b.+\bclubs\b/i, @ace_of_clubs.to_s)
+	end
+end
