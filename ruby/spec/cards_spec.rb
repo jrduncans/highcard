d0394cdfe3c4c31c3c95a63191e9e437f153b597
stephen.duncan <stephen.duncan@f191c422-131a-0410-8d44-f1ddf0d51770>
2007-05-15 19:46:14 +0000

    Converted spec to latest syntax

--- ruby/spec/cards_spec.rb
+++ ruby/spec/cards_spec.rb
@@ -1,76 +1,76 @@
 require 'cards'
 
-context 'An Ace of Clubs' do
-  setup do
+describe 'An Ace of Clubs' do
+  before(:each) do
     @card = Cards::Card.new('Ace', 'Clubs')
   end
 
-  specify "should include 'ace' and 'clubs' in display" do
-    @card.to_s.should_match /\bace\b.+\bclubs\b/i
+  it "should include 'ace' and 'clubs' in display" do
+    @card.to_s.should match(/\bace\b.+\bclubs\b/i)
   end
 
-  specify 'should equal another Ace of Clubs' do
-    @card.should_eql Cards::Card.new('Ace', 'Clubs')
+  it 'should equal another Ace of Clubs' do
+    @card.should eql(Cards::Card.new('Ace', 'Clubs'))
   end
 
-  specify 'should not equal an Ace of Spades' do
-    @card.should_not_eql Cards::Card.new('Ace', 'Spades')
+  it 'should not equal an Ace of Spades' do
+    @card.should_not eql(Cards::Card.new('Ace', 'Spades'))
   end
 
-  specify 'should not equal a Two of Clubs' do
-    @card.should_not_eql Cards::Card.new(2, 'Clubs')
+  it 'should not equal a Two of Clubs' do
+    @card.should_not eql(Cards::Card.new(2, 'Clubs'))
   end
 
-  specify 'should be less than a Two of Clubs' do
-    @card.should_be < Cards::Card.new(2, 'Clubs')
+  it 'should be less than a Two of Clubs' do
+    @card.should satisfy {|card| card < Cards::Card.new(2, 'Clubs')}
   end
 
-  specify 'should compare equally to an Ace of Spades' do
-    (@card <=> Cards::Card.new('Ace', 'Spades')).should_equal 0
+  it 'should compare equally to an Ace of Spades' do
+    (@card <=> Cards::Card.new('Ace', 'Spades')).should equal(0)
   end
 end
 
-context 'A 3 of Diamonds' do
-  setup do
+describe 'A 3 of Diamonds' do
+  before(:each) do
     @card = Cards::Card.new(3, 'Diamonds')
   end
 
-  specify 'should be greater than the Two of Clubs' do
-    @card.should_be > Cards::Card.new(2, 'Clubs')
+  it 'should be greater than the Two of Clubs' do
+    @card.should satisfy {|card| card > Cards::Card.new(2, 'Clubs')}
   end
 end
 
 require 'set'
 
-context 'A new Deck' do
-  setup do
+describe 'A new Deck' do
+  before(:each) do
     @deck = Cards::Deck.new
   end
 
-  specify 'should have 52 unique cards' do
+  it 'should have 52 unique cards' do
     set = Set.new
 
     52.times do
       set << @deck.draw
     end
 
-    set.size.should_equal 52
+    set.size.should equal(52)
   end
 end
 
-context 'An empty Deck' do
-  setup do
+describe 'An empty Deck' do
+  before(:each) do
     @deck = Cards::Deck.new
 
     52.times { @deck.draw }
   end
 
-  specify 'should have 52 cards after resetting' do
+  it 'should have 52 cards after resetting' do
     @deck.reset
 
     cards = []
     52.times { cards << @deck.draw }
 
-    cards.length.should_equal 52
+    cards.length.should equal(52)
   end
 end
